e8745c39b5f87c64972e311e7a5d733fcb7b52d8
Randall Wilk <wilk.randall@gmail.com>
2023-11-18 18:42:48 -0600

    Wrap public key generation in a timebox

--- src/Http/Controllers/PasskeysController.php
+++ src/Http/Controllers/PasskeysController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Rawilk\ProfileFilament\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Timebox;
 use Rawilk\ProfileFilament\Enums\Session\MfaSession;
 use Rawilk\ProfileFilament\Facades\Webauthn;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,23 +15,41 @@ class PasskeysController
 {
     public function assertionPublicKey(): Response
     {
-        $publicKey = Webauthn::passkeyAssertionObject();
+        $publicKey = App::make(Timebox::class)->call(callback: function (Timebox $timebox) {
+            $publicKey = Webauthn::passkeyAssertionObject();
 
-        session()->put(MfaSession::PasskeyAssertionPk->value, serialize($publicKey));
+            session()->put(
+                MfaSession::PasskeyAssertionPk->value,
+                serialize($publicKey),
+            );
+
+            $timebox->returnEarly();
+
+            return $publicKey;
+        }, microseconds: 300 * 1000);
 
         return response()->json($publicKey->jsonSerialize());
     }
 
     public function attestationPublicKey(Request $request): Response
     {
-        $model = config('profile-filament.models.webauthn_key');
-        $publicKey = Webauthn::passkeyAttestationObjectFor(
-            username: app($model)::getUsername(auth()->user()),
-            userId: app($model)::getUserHandle(auth()->user()),
-            excludeCredentials: $request->get('exclude', []),
-        );
+        $publicKey = App::make(Timebox::class)->call(callback: function (Timebox $timebox) use ($request) {
+            $model = config('profile-filament.models.webauthn_key');
+            $publicKey = Webauthn::passkeyAttestationObjectFor(
+                username: app($model)::getUsername(auth()->user()),
+                userId: app($model)::getUserHandle(auth()->user()),
+                excludeCredentials: $request->get('exclude', []),
+            );
 
-        session()->put(MfaSession::PasskeyAttestationPk->value, serialize($publicKey));
+            session()->put(
+                MfaSession::PasskeyAttestationPk->value,
+                serialize($publicKey),
+            );
+
+            $timebox->returnEarly();
+
+            return $publicKey;
+        }, microseconds: 300 * 1000);
 
         return response()->json($publicKey->jsonSerialize());
     }
--- src/Http/Controllers/WebauthnPublicKeysController.php
+++ src/Http/Controllers/WebauthnPublicKeysController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Rawilk\ProfileFilament\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Timebox;
 use Rawilk\ProfileFilament\Enums\Session\MfaSession;
 use Rawilk\ProfileFilament\Facades\Webauthn;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,30 +15,45 @@ class WebauthnPublicKeysController
 {
     public function assertionPublicKey($user, Request $request): Response
     {
-        $user = app(config('auth.providers.users.model'))::findOrFail($user);
+        $publicKey = App::make(Timebox::class)->call(callback: function (Timebox $timebox) use ($user, $request) {
+            $user = app(config('auth.providers.users.model'))::findOrFail($user);
 
-        $model = config('profile-filament.models.webauthn_key');
-        $publicKey = Webauthn::assertionObjectFor(
-            userId: app($model)::getUserHandle($user),
-        );
+            $model = config('profile-filament.models.webauthn_key');
+            $publicKey = Webauthn::assertionObjectFor(
+                userId: app($model)::getUserHandle($user),
+            );
 
-        session()->put(
-            $request->get('s') ?? MfaSession::AssertionPublicKey->value,
-            serialize($publicKey),
-        );
+            session()->put(
+                $request->get('s') ?? MfaSession::AssertionPublicKey->value,
+                serialize($publicKey),
+            );
+
+            $timebox->returnEarly();
+
+            return $publicKey;
+        }, microseconds: 300 * 1000);
 
         return response()->json($publicKey->jsonSerialize());
     }
 
     public function attestationPublicKey(): Response
     {
-        $model = config('profile-filament.models.webauthn_key');
-        $publicKey = Webauthn::attestationObjectFor(
-            username: app($model)::getUsername(auth()->user()),
-            userId: app($model)::getUserHandle(auth()->user()),
-        );
+        $publicKey = App::make(Timebox::class)->call(callback: function (Timebox $timebox) {
+            $model = config('profile-filament.models.webauthn_key');
+            $publicKey = Webauthn::attestationObjectFor(
+                username: app($model)::getUsername(auth()->user()),
+                userId: app($model)::getUserHandle(auth()->user()),
+            );
 
-        session()->put(MfaSession::AttestationPublicKey->value, serialize($publicKey));
+            session()->put(
+                MfaSession::AttestationPublicKey->value,
+                serialize($publicKey),
+            );
+
+            $timebox->returnEarly();
+
+            return $publicKey;
+        }, microseconds: 300 * 1000);
 
         return response()->json($publicKey->jsonSerialize());
     }
