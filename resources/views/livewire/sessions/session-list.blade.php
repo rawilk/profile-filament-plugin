4c719615c0180a57cb94ee24293a16050cfbbabb
Randall Wilk <wilk.randall@gmail.com>
2024-10-09 12:07:43 -0500

    Refactor session manager

--- resources/views/livewire/sessions/session-list.blade.php
+++ resources/views/livewire/sessions/session-list.blade.php
@@ -1,15 +1,16 @@
-<p class="mt-4 text-sm">{{ __('profile-filament::pages/sessions.manager.list_description') }}</p>
+<p class="text-sm">{{ __('profile-filament::pages/sessions.manager.list_description') }}</p>
 
 <div class="mt-4 divide-y border border-gray-300 dark:border-gray-600 rounded-md divide-gray-300 dark:divide-gray-600">
     @foreach ($this->sessions as $session)
-        <div wire:key="session-{{ $session->id }}"
-             class="first:rounded-t-md last:rounded-b-md bg-slate-50 dark:bg-gray-800"
+        <div
+            wire:key="session.{{ $session->id }}"
+            class="first:rounded-t-md last:rounded-b-md bg-gray-50 dark:bg-gray-800"
         >
             <div class="px-6 py-4">
                 <div class="flex justify-between items-start gap-x-4">
                     <div>
                         <div class="flex gap-x-4">
-                            <div>
+                            <div class="shrink-0">
                                 <x-filament::icon
                                     :alias="$session->agent->isDesktop() ? 'session::desktop' : 'session::mobile'"
                                     :icon="$session->agent->isDesktop() ? 'heroicon-o-computer-desktop' : 'heroicon-o-device-phone-mobile'"
@@ -17,7 +18,7 @@
                                 />
                             </div>
 
-                            <div>
+                            <div class="flex-1">
                                 <div class="text-sm">
                                     {{ $session->agent->platform() ?: __('profile-filament::pages/sessions.manager.unknown_platform') }}
                                     -
@@ -35,7 +36,7 @@
                                         {{ $session->ip_address }}
                                     </x-filament::link>
 
-                                    <span>-</span>
+                                    <span aria-hidden="true">-</span>
 
                                     @if ($session->is_current_device)
                                         <span class="text-success-500 font-semibold">{{ __('profile-filament::pages/sessions.manager.current_device') }}</span>
@@ -47,9 +48,10 @@
                         </div>
                     </div>
 
+                    {{-- actions --}}
                     <div>
                         @unless ($session->is_current_device)
-                            {{ ($this->revokeAction)(['session' => $session->id]) }}
+                            {{ ($this->revokeSessionAction)(['record' => $session->id]) }}
                         @endunless
                     </div>
                 </div>
