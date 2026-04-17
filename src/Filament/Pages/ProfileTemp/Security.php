16d41491617004dacc0ae77a1f8ff3a0a37e849f
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2026-04-17 15:07:00 -0500

    wip

---
+++ src/Filament/Pages/ProfileTemp/Security.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rawilk\ProfileFilament\Filament\Pages\ProfileTemp;
+
+use Illuminate\Contracts\Support\Htmlable;
+use Rawilk\ProfileFilament\Concerns;
+use Rawilk\ProfileFilament\Filament\Pages\ProfileTemp\ProfilePage;
+use Rawilk\ProfileFilament\ProfileFilamentPlugin;
+
+class Security extends ProfilePage
+{
+    use Concerns\HasPanelPageRoutes;
+
+    public static function getNavigationLabel(): string
+    {
+        return __('profile-filament::pages/security.title');
+    }
+
+    public function getTitle(): string|Htmlable
+    {
+        return __('profile-filament::pages/security.title');
+    }
+
+    protected function needsSudoChallengeForm(): bool
+    {
+        return filament(ProfileFilamentPlugin::PLUGIN_ID)->hasSudoMode();
+    }
+}
